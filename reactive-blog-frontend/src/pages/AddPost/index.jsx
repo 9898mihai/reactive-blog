2197d2d68ad1decf124c0e42ba878e14833a9071
mihai9898 <faktxd@gmail.com>
2022-10-04 16:57:20 +0300

    upload image in post

--- reactive-blog-frontend/src/pages/AddPost/index.jsx
+++ reactive-blog-frontend/src/pages/AddPost/index.jsx
@@ -1,4 +1,4 @@
-import { React, useState, useCallback, useMemo } from 'react';
+import { React, useState, useCallback, useMemo, useRef } from 'react';
 import TextField from '@mui/material/TextField';
 import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
@@ -7,18 +7,31 @@ import SimpleMDE from 'react-simplemde-editor';
 import 'easymde/dist/easymde.min.css';
 import { useSelector } from 'react-redux';
 import { selectIsAuth } from '../../redux/slices/auth';
+import axios from '../../axios';
 import { Navigate } from 'react-router-dom';
 
 import styles from './AddPost.module.scss';
 
 export const AddPost = () => {
-  const imageUrl = '';
   const isAuth = useSelector(selectIsAuth);
   const [value, setValue] = useState('');
   const [title, setTitle] = useState('');
   const [tags, setTags] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+  const inputFileRef = useRef(null);
 
-  const handleChangeFile = () => {};
+  const handleChangeFile = async (event) => {
+    try {
+      const formData = new FormData();
+      const file = event.target.files[0];
+      formData.append('image', file);
+      const { data } = await axios.post('/upload', formData);
+      setImageUrl(data.url);
+    } catch (err) {
+      console.warn(err);
+      alert('Error uploading file!');
+    }
+  };
 
   const onClickRemoveImage = () => {};
 
@@ -47,21 +60,35 @@ export const AddPost = () => {
 
   return (
     <Paper style={{ padding: 30 }}>
-      <Button variant="outlined" size="large">
+      <Button
+        onClick={() => inputFileRef.current.click()}
+        variant="outlined"
+        size="large"
+      >
         Download preview
       </Button>
-      <input type="file" onChange={handleChangeFile} hidden />
+      <input
+        ref={inputFileRef}
+        type="file"
+        onChange={handleChangeFile}
+        hidden
+      />
       {imageUrl && (
-        <Button variant="contained" color="error" onClick={onClickRemoveImage}>
-          Delete
-        </Button>
-      )}
-      {imageUrl && (
-        <img
-          className={styles.image}
-          src={`http://localhost:4444${imageUrl}`}
-          alt="Uploaded"
-        />
+        <>
+          {' '}
+          <Button
+            variant="contained"
+            color="error"
+            onClick={onClickRemoveImage}
+          >
+            Delete
+          </Button>
+          <img
+            className={styles.image}
+            src={`http://localhost:4444${imageUrl}`}
+            alt="Uploaded"
+          />
+        </>
       )}
       <br />
       <br />
